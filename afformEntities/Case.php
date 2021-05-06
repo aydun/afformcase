b06f3847542ed960fc62262c79ec0c44b9c64ded
Aidan Saunders <aidan.saunders@squiffle.uk>
2021-05-06 13:24:48 +0100

    Enhance defaults

--- afformEntities/Case.php
+++ afformEntities/Case.php
@@ -1,7 +1,13 @@
 <?php
 return [
   'entity' => 'Case',
-  'defaults' => "{'url-autofill':'1'}",
+  'defaults' => "{
+    data: {
+      case_type_id: '',
+      creator_id: 'user_contact_id'
+    },
+    'url-autofill': '1'
+  }",
   'boilerplate' => [
     ['#tag' => 'af-field', 'name' => 'subject'],
   ],
